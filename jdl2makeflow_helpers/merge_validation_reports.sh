5670e3a17c5b9faf4027872325655c67023f2290
Dario Berzano <dario.berzano@cern.ch>
2018-05-20 16:13:29 +0200

    Append line num in report, do not prepend path
    Not prepending path allows for easier relocation of output data. We also make
    sure reports and corefiles are outputs of every job, even if not in the JDL.

--- jdl2makeflow_helpers/merge_validation_reports.sh
+++ jdl2makeflow_helpers/merge_validation_reports.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
-mkdir mergelogs
 cat > copyHere.C <<EOF
 void copyHere() {
   Int_t count = 0;
-  TString dest;
-$(cat "$1" | xargs -n1 -IXXX echo -e "  dest = \"mergelogs/log\"; dest += count++;\n  TFile::Cp(\"XXX\", dest.Data());")
+  TString cmd;
+$(cat "$1" | xargs -n1 -IXXX echo -e "\n  gSystem->Unlink(\"temp.txt\");\n  cmd.Form(\"cat temp.txt | xargs -n1 -I{} echo '%s/{}' >> validation_report_full.txt\", gSystem->DirName(\"XXX\"));\n  TFile::Cp(\"XXX\", \"temp.txt\");\n  gSystem->Exec(cmd.Data());")
 }
 EOF
 root -l -b -q copyHere.C
-cat mergelogs/* | sed -e '/^$/d' > validation_report_full.txt
-rm -rf mergelogs copyHere.C
+cat validation_report_full.txt | sed -e '/^$/d' | sort > validation_report_full.txt.0
+mv validation_report_full.txt.0 validation_report_full.txt
+rm -rf temp.txt copyHere.C
